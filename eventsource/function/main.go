8eda17703c40c6e31d74acd353de545c36ff9527
laminar <fangtian@kubesphere.io>
2021-09-02 15:44:07 +0800

    Add handler function
    Signed-off-by: laminar <fangtian@kubesphere.io>

---
+++ eventsource/function/main.go
@@ -0,0 +1,75 @@
+package eventsource
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"os"
+	"strings"
+
+	ofctx "github.com/OpenFunction/functions-framework-go/openfunction-context"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	config Config
+)
+
+type Config struct {
+	EventBusOutputName string `json:"eventBusOutputName,omitempty"`
+	EventBusTopic      string `json:"eventBusTopic,omitempty"`
+	SinkOutputName     string `json:"sinkOutputName,omitempty"`
+	LogLevel           string `json:"logLevel,omitempty"`
+	Port               string `json:"port,omitempty"`
+}
+
+func getEnvVar(key, fallbackValue string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return strings.TrimSpace(val)
+	}
+	return fallbackValue
+}
+
+func init() {
+	encodedConfig := getEnvVar("CONFIG", "")
+	if len(encodedConfig) > 0 {
+		envConifgSpec, err := base64.StdEncoding.DecodeString(encodedConfig)
+		if err != nil {
+			log.Fatalf("failed to decode config string: %v", err)
+		}
+		if err = json.Unmarshal(envConifgSpec, &config); err != nil {
+			log.Fatalf("failed to unmarshal config object: %v", err)
+		}
+	}
+
+	if config.Port == "" {
+		config.Port = "5050"
+	}
+
+	switch config.LogLevel {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.SetLevel(log.DebugLevel)
+	}
+}
+
+func EventSourceHandler(ctx *ofctx.OpenFunctionContext, in []byte) ofctx.RetValue {
+	var err error
+	if config.EventBusOutputName != "" {
+		_, err = ctx.Send(config.EventBusOutputName, in)
+		log.Error(err)
+	}
+	if config.SinkOutputName != "" {
+		_, err = ctx.Send(config.SinkOutputName, in)
+		log.Error(err)
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	return ctx.ReturnWithSuccess()
+}
---
+++ trigger/function/go.mod
@@ -0,0 +1,13 @@
+module openfunction.io/trigger
+
+go 1.15
+
+require (
+	github.com/OpenFunction/functions-framework-go v0.0.0-20210922063920-81a7b2951b8a
+	github.com/dapr/go-sdk v1.2.0
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/google/cel-go v0.7.3
+	github.com/sirupsen/logrus v1.8.1
+	google.golang.org/genproto v0.0.0-20201204160425-06b3db808446
+	google.golang.org/protobuf v1.25.0
+)
